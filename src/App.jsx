c0c3f241369b9d3849a6dee5ea105892cd2fd452
makhnev-a <ray.makhnev@gmail.com>
2020-07-11 23:18:44 +0300

    task2

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import './App.css';
 import Message from "./Message/Message";
 
 function App() {
+    const qualities = [
+        'рационалист',
+        'комуннист',
+        'анимешник'
+    ];
+
     return (
         <div className="App">
             <Message/>
+            {qualities.map((q, index) => {
+                if (index + 1 === 2) {
+                    return <span key={`qualities${index}`} className={'qualitiesSecond'}>{`${index + 1} качество = ${q}`}</span>;
+                }
+
+                return <span key={`qualities${index}`} className={'qualities'}>{`${index + 1} качество = ${q}`}</span>;
+            })}
         </div>
     );
 }
